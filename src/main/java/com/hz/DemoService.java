a4f1373e8d9b57eabdcbb4230d1bc0e242c25e47
隔壁老王 <269937035@qq.com>
2018-03-07 18:18:56 +0800

    Initial commit

---
+++ src/main/java/com/hz/DemoService.java
@@ -0,0 +1,10 @@
+package com.hz;
+
+/**
+ * @author HuangZhu
+ * @version V1.0
+ * @Description: 接口
+ */
+public interface DemoService {
+    String sayHello(String name);
+}
---
+++ src/main/java/com/hz/boot/dubbo/run/DubboConsumerDemo.java
@@ -0,0 +1,13 @@
+package com.hz.boot.dubbo.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.hz.boot.dubbo.demo")
+public class DubboConsumerDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboConsumerDemo.class,args);
+    }
+
+}
